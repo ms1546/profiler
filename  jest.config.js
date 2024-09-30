aee6c75f11253730c87037d5887484d7e6ac444a
ms <maemotosato@gmail.coms>
2024-09-30 23:08:52 +0900

    fix

---  jest.config.js
+++  jest.config.js
@@ -1,10 +1,11 @@
 module.exports = {
-    preset: 'ts-jest',
-    testEnvironment: 'jsdom',
-    transform: {
-      '^.+\\.tsx?$': 'ts-jest',
-    },
-    moduleNameMapper: {
-      '^@/(.*)$': '<rootDir>/src/$1',
-    },
-  };
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+  },
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+  roots: ['<rootDir>/src'],
+};
